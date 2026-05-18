e16225f0dd0dbf279e540e296eef5a7ec38dff17
modiyamdeepika1234-hue <modiyamdeepika1234@gmail.com>
2026-05-18 15:47:22 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,12 @@ app.get("/", (req, res) => {
     message: "Backend running",
   });
 });
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/auth', require('./routes/auth.routes'));
+app.use('/api/users', require('./routes/user.routes'));
+app.use('/api/notifications', require('./routes/notification.routes'));
+app.use('/api/connections', require('./routes/connection.routes'));
+app.use('/api/messages', require('./routes/message.routes'));
+app.use('/api/posts', require('./routes/post.routes'));
 (async () => {
   await connectDB();
   const server = http.createServer(app);
